1fd211e996fe877c2863d6f260291e245040c84e
Luc van Donkersgoed <lucvandonkersgoed@gmail.com>
2020-11-13 12:32:56 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Setup script for aws-nsm-interface."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aws-nsm-interface",
+    version="0.0.1",
+    author="Luc van Donkersgoed",
+    author_email="lucvandonkersgoed+python@gmail.com",
+    description="Native Python interface for the AWS Nitro Secure Module (NSM)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donkersgoed/aws-nsm-interface",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
